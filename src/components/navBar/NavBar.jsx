6d4f688b76d7bb7128eb41342f95148b6de7b6ec
ameentm24 <toameentm@gmail.com>
2025-01-16 15:05:49 +0530

    navbar items

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -7,13 +7,13 @@ import "./navbar.css"
 const NavBar = () => {
   return (
     <>
-      <nav class="navbar navbar-expand-lg px-lg-4 px-2 ">
-        <div class="container-fluid p-2 px-lg-5">
-          <a class="navbar-brand" href="#">
+      <nav className="navbar navbar-expand-lg px-lg-4 px-2 ">
+        <div className="container-fluid p-2 px-lg-5">
+          <a className="navbar-brand" href="#">
             <img width={100} src="/images/lebonLogo.png" alt="" />
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -21,34 +21,49 @@ const NavBar = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            {/* <span class="navbar-toggler-icon"></span> */}
+            {/* <span className="navbar-toggler-icon"></span> */}
 <img className="navbar-toggler-icon" src="/images/menu.png" alt="menu" />
             
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="#">
+                  New Arrivals
+                </NavLink>
+              </li>
+
+              {/* ========= */}
+              <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li>< className="dropdown-divider"></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+              {/* ============ */}
+              <li className="nav-item">
+                <NavLink className="nav-link" to="#">
+                  Pastries
+                </NavLink>
+              </li>
               <li className="nav-item">
                 <NavLink className="nav-link" to="#">
                   Menu
                 </NavLink>
               </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="#">
-                  Find Dining
-                </NavLink>
-              </li>
+
               <li className="nav-item">
                 <NavLink className="nav-link" to="#">
                   About
                 </NavLink>
               </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="#">
-                  Contact
-                </NavLink>
-              </li>
             </ul>
-            <div class="d-flex">
+            <div className="d-flex">
               <Button
                 className="button txt-yellow"
                 divClass="nav-item bt-container"
