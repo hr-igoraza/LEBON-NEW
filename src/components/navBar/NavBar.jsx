62b102aa96cca42269d271662148b0bfff00056d
developerigoraza <toameentm@gmail.com>
2025-01-16 15:14:47 +0530

    rm dropdown

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -34,17 +34,16 @@ const NavBar = () => {
               </li>
 
               {/* ========= */}
-              <li className="nav-item dropdown">
+              {/* <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
             <li><a className="dropdown-item" href="#">Action</a></li>
             <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li>< className="dropdown-divider"></li>
             <li><a className="dropdown-item" href="#">Something else here</a></li>
           </ul>
-        </li>
+        </li> */}
               {/* ============ */}
               <li className="nav-item">
                 <NavLink className="nav-link" to="#">
