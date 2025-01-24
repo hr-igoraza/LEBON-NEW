e81850372eced41a8f8c5b7a2e1db0f0a62cbd9e
developerigoraza <toameentm@gmail.com>
2025-01-24 21:12:55 +0530

    api update

--- src/components/tabCards/TabCards.jsx
+++ src/components/tabCards/TabCards.jsx
@@ -43,8 +43,11 @@ const TabCards = (props) => {
   return (
     <>
       <div className="tab-card">
-        <div className="cardImg">
-          <img src={props.img} alt={props.title} />
+        <div className="cardImg"
+        
+        style={{background:`${url(props.img)}`}}
+        >
+          {/* <img src={props.img} alt={props.title} /> */}
         </div>
         <div className="tab-item-details">
           <h3 className="card-item-name f-3">
