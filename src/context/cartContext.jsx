35b3dce6f68df39e2f3e3956fac739ea89f7d455
ameentm24 <toameentm@gmail.com>
2025-02-05 18:23:12 +0530

    cart

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -1,3 +1,28 @@
+// import React, { createContext, useState } from "react";
+
+// export const CartContext = createContext();
+
+// export const CartProvider = ({ children }) => {
+//   const [cart, setCart] = useState([]);
+
+  
+//   const addToCart = (item) => {
+//     setCart((prevCart) => [...prevCart, item]);
+//   };
+
+ 
+//   const removeFromCart = (itemId) => {
+//     setCart((prevCart) => prevCart.filter((item) => item.id !== itemId));
+//   };
+
+//   return (
+//     <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+//       {children}
+//     </CartContext.Provider>
+//   );
+// };
+
+
 import React, { createContext, useState } from "react";
 
 export const CartContext = createContext();
@@ -5,14 +30,12 @@ export const CartContext = createContext();
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
-  
   const addToCart = (item) => {
-    setCart((prevCart) => [...prevCart, item]);
+    setCart([item]); // Always replace cart with the latest item
   };
 
- 
-  const removeFromCart = (itemId) => {
-    setCart((prevCart) => prevCart.filter((item) => item.id !== itemId));
+  const removeFromCart = () => {
+    setCart([]); // Clears the cart
   };
 
   return (
