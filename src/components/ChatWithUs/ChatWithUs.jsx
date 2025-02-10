740cc10ab7034909c79b6ee83da7331ba032b57b
ameentm24 <toameentm@gmail.com>
2025-02-10 14:45:48 +0530

    icon

--- src/components/ChatWithUs/ChatWithUs.jsx
+++ src/components/ChatWithUs/ChatWithUs.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./chatWithUs.css";
-import chat from "/images/footer/ChatWithUs.svg";
+import chat from "/images/footer/chatbox.svg";
+import whatsapp from "/images/footer/whatsappIcon.svg";
 import { adminPhoneNumber } from "../../utils/api";
 
 const ChatWithUs = () => {
@@ -8,13 +9,29 @@ const ChatWithUs = () => {
     <>
       <div className="footer-whatsapp">
         <a
+        className="chat-with-us"
           href={`https://wa.me/${adminPhoneNumber}?text=Hi,%20I%20want%20to%20know%20more%20about%20Lebon`}
           target="_blank"
           rel="noopener noreferrer"
         >
           <img src={chat} alt="Chat With Us" />
         </a>
+
+        <a
+        className="chat-with-us-icon"
+          href={`https://wa.me/${adminPhoneNumber}?text=Hi,%20I%20want%20to%20know%20more%20about%20Lebon`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={whatsapp} alt="Chat With Us" />
+        </a>
+
       </div>
+
+      {/* ==== */}
+
+
+      {/* ==== */}
     </>
   );
 };
